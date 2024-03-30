e2caa1614e9b40970c6fd1b14a8a8bd55f81af68
divyasreenivasulu-hub <84560548+divyasreenivasulu-hub@users.noreply.github.com>
2024-03-30 19:47:56 +0000

    add

--- part4.py
+++ part4.py
@@ -1,14 +1,15 @@
 import time
 import warnings
 import numpy as np
+import myplots as myplt
 import matplotlib.pyplot as plt
 from sklearn import cluster, datasets, mixture
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs,make_circles,make_moons
 from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 import scipy.io as io
-from scipy.cluster.hierarchy import dendrogram, linkage  #
+from scipy.cluster.hierarchy import dendrogram, linkage,fcluster  #
 
 # import plotly.figure_factory as ff
 import math
@@ -26,12 +27,40 @@ In this task, you will explore hierarchical clustering over different datasets. 
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_hierarchical_cluster():
-    return None
+def fit_hierarchical_cluster(data, n_clusters, linkage_type):
+    scaler=StandardScaler()
+    data_standardized = scaler.fit_transform(data[0])
 
-def fit_modified():
-    return None
+    # Fit the AgglomerativeClustering model
+    clustering = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage_type)
+    clustering.fit(data_standardized,data[1])
+    labels=clustering.labels_
+    return labels
+    #return None
 
+def fit_modified(data, linkage_type):
+    scaler = StandardScaler()
+    data_standardized = scaler.fit_transform(data[0])
+
+    # Calculate the linkage matrix
+    Z = linkage(data_standardized, linkage=linkage_type)
+
+    # Find the maximum rate of change in distances between successive merges
+    distances = Z[:, 2]
+    distance_diff = np.diff(distances)
+    max_diff_index = np.argmax(distance_diff)
+
+    # Define the cut-off distance
+    cut_off_distance = distances[max_diff_index] + distance_diff[max_diff_index] / 2
+
+    # Number of clusters is the number of merges above the cut-off distance plus 1
+    n_clusters = len(distances[distances > cut_off_distance]) + 1
+
+    # Fit hierarchical clustering with calculated number of clusters
+    model = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage_type)
+    model.fit(data_standardized)
+
+    return model.labels_
 
 def compute():
     answers = {}
@@ -39,10 +68,27 @@ def compute():
     """
     A.	Repeat parts 1.A and 1.B with hierarchical clustering. That is, write a function called fit_hierarchical_cluster (or something similar) that takes the dataset, the linkage type and the number of clusters, that trains an AgglomerativeClustering sklearn estimator and returns the label predictions. Apply the same standardization as in part 1.B. Use the default distance metric (euclidean) and the default linkage (ward).
     """
+    n_samples = 100
+    random_state = 42
+
+# Generate the datasets
+    nc = datasets.make_circles(n_samples=n_samples, factor=0.5, noise=0.05, random_state=random_state)
+    nm = datasets.make_moons(n_samples=n_samples, noise=0.05, random_state=random_state)
+    b = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+    bvv = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=random_state)
+
+    # Anisotropicly distributed data
+    X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
+    add = np.dot(X, transformation)
 
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
-    dct = answers["4A: datasets"] = {}
+    dct = answers["4A: datasets"] = {'nc': [nc[0],nc[1]],
+                                     'nm': [nm[0],nm[1]],
+                                     'bvv': [bvv[0],bvv[1]],
+                                     'add': [add[0],add[1]],
+                                     'b': [b[0],b[1]]}
 
     # dct value:  the `fit_hierarchical_cluster` function
     dct = answers["4A: fit_hierarchical_cluster"] = fit_hierarchical_cluster
@@ -52,6 +98,14 @@ def compute():
 
     Create a pdf of the plots and return in your report. 
     """
+    #modified_dct = {}
+
+# Assume 'dct' contains your datasets as before
+    linkage_types = ['single', 'complete', 'ward', 'average']
+    for i, (linkage_type) in enumerate(linkage_types, 1):
+        for j, (dataset_name, (X, _)) in enumerate(answers["4A: datasets"].items(), 1):
+            labels = fit_hierarchical_cluster(X, n_clusters=2, linkage_type=linkage_types)
+    myplt.plot_part1C(labels,'part4b.jpg')
 
     # dct value: list of dataset abbreviations (see 1.C)
     dct = answers["4B: cluster successes"] = [""]
